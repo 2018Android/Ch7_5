24d96deacd2bef9fc43d7bf58aef9c4bb1eda06b
chenyirou <annie851216@gmail.com>
2018-05-08 11:04:24 +0800

    test

--- app/src/main/java/com/example/admin/ch7_5/MainActivity.java
+++ app/src/main/java/com/example/admin/ch7_5/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.admin.ch7_5;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    //建立物件
+    FisrtFragment ff = new FisrtFragment();
+    SecondFragment sf = new SecondFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //將第一個Fragment 片段加入frame中
+        getSupportFragmentManager().beginTransaction().add(R.id.frame, ff).commit();
+    }
+    //當按下取代第二個Fragement片段，frame顯示為第二個Fragement片段
+    public void button_Click(View v){
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame, sf).commit();
+    }
+    //當按下刪除第二個Fragement片段，frame刪除第二個Fragement片段
+    public void button1_Click(View v){
+        getSupportFragmentManager().beginTransaction().remove(sf).commit();
     }
 }
