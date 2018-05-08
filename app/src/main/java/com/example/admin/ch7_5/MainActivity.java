66cc1a4dae51832d8bf86418c6a3a5ce35e3ad9c
chenyirou <annie851216@gmail.com>
2018-05-08 11:59:12 +0800

    onClick事件

--- app/src/main/java/com/example/admin/ch7_5/MainActivity.java
+++ app/src/main/java/com/example/admin/ch7_5/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     //建立物件
     FisrtFragment ff = new FisrtFragment();
     SecondFragment sf = new SecondFragment();
+    SecondFragment sfi = new SecondFragment().newInstance("自帶參數的第二個Fragement片段");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +25,9 @@ public class MainActivity extends AppCompatActivity {
     //當按下刪除第二個Fragement片段，frame刪除第二個Fragement片段
     public void button1_Click(View v){
         getSupportFragmentManager().beginTransaction().remove(sf).commit();
+    }
+    //當按下自帶參數片段，frame顯示為自帶參數的第二個Fragement片段
+    public void button2_Click(View v){
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame, sfi).commit();
     }
 }
